555fd96778021e8e5a1e3ae476c13e1718d42ddb
kejdaC <34389523+kejdaC@users.noreply.github.com>
2019-04-21 01:31:52 -0400

    initial
    initial

---
+++ sample.py
@@ -0,0 +1,6 @@
+import django
+
+print(django.get_version())  #checking if django works
+
+#django-admin startproject forgewood (starts a new project from the terminal)
+#python manage.py runserver (runs server)
